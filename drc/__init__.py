1bc32ae305c08461141c70bfa89542e0abbf4b95
Sergey <xxsmotur@gmail.com>
2018-10-21 17:17:03 +0500

    Add method for send get request

--- drc/__init__.py
+++ drc/__init__.py
@@ -8,3 +8,12 @@ class Core:
     @property
     def token(self):
         return self_token
+    
+    def repositories(self):
+        NotImplemented
+    
+    def _send_request_get(self, url):
+        r = requests.get(url)
+        status_code = r.status_code
+        if status_code is not 200:
+            raise Exception("Unable to send get request")
